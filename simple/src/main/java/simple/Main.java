1ff6ca7338de652822a8125a2e32e6caf26b1341
Emanuele Galli <egalli64@gmail.com>
2016-10-25 16:02:01 +0200

    http://bitingcode.blogspot.com/2016/10/adding-logback-to-maven-project.html

--- simple/src/main/java/simple/Main.java
+++ simple/src/main/java/simple/Main.java
@@ -1,9 +1,16 @@
+/**
+ * Simple Java Maven project with dependencies
+ * More info http://bitingcode.blogspot.com/2016/10/adding-logback-to-maven-project.html
+ */
 package simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        log.info("Hello");
     }
-
 }
