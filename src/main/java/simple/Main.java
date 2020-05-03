33d86b7c64dd9810cad20c0bef7dca3d5be29a8f
Manny <egalli64@gmail.com>
2020-05-03 13:44:58 +0200

    jar-with-dependencies example

--- src/main/java/simple/Main.java
+++ src/main/java/simple/Main.java
@@ -1,7 +1,13 @@
 package simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("Hello");
+        LOG.info("Hello");
     }
 }
